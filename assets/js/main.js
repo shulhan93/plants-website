4241b9b34d74b435546e5bf200eed4817bd9064d
shulhan93 <shulhan.andrei@gmail.com>
2023-02-15 10:59:54 +0300

    added scrollreveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,8 +150,8 @@ themeButton.addEventListener("click", () => {
 const sr = ScrollReveal({
     origin: "top",
     distance: "60px",
-    duration: 2500,
-    delay: 400,
+    duration: 2000,
+    delay: 300,
     // reset: true
 });
 
